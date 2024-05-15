4a53354458e81445bd5c1c04c1e309441f40a573
darinhuwaidaa <darinhuwaida21@gmail.com>
2024-05-15 15:34:15 +0700

    Update avgPseudo.R

--- R/avgPseudo.R
+++ R/avgPseudo.R
@@ -49,133 +49,135 @@ avgPseudo <- function(prov, reg, sub, vill, y, x, var, N, method="REML") {
   if (any(is.na(N)))
     stop("Argument N contains NA values.")
 
-  # Select method for SAE
-  if (method=="REML") {
-    sae_result <- mseFH(y ~ x, var, method = "REML")
-  } else if (method=="ML") {
-    sae_result <- mseFH(y ~ x, var, method = "ML")
-  } else if (method=="FH") {
-    sae_result <- mseFH(y ~ x, var, method = "FH")
+  if (!any(is.na(N))){
+    # Select method for SAE
+    if (method=="REML") {
+      sae_result <- mseFH(y ~ x, var, method = "REML")
+    } else if (method=="ML") {
+      sae_result <- mseFH(y ~ x, var, method = "ML")
+    } else if (method=="FH") {
+      sae_result <- mseFH(y ~ x, var, method = "FH")
+    }
+
+    # Extract eblup est, mse, and rse
+    y_eblup <- sae_result$est$eblup
+    mse_eblup <- sae_result$mse
+    rse_eblup <- (sqrt(mse_eblup) / y_eblup)*100
+
+    # Create the eblup_data dataset
+    eblup_data <- data.frame(Village = vill, Y_eblup = y_eblup, MSE_eblup = mse_eblup, RSE_eblup = rse_eblup)
+
+    # Create the sae_vill dataset
+    sae_vill <- data.frame(Province = prov, Region = reg, Subdistrict = sub, N_vill = N)
+
+    # Join the sae_vill dataset with eblup_data
+    sae_vill <- bind_cols(sae_vill, eblup_data)
+
+    # Calculate N_sub (amount of data (population) in each Subdistrict)
+    N_sub <- sae_vill %>%
+      group_by(Province, Region, Subdistrict) %>%
+      summarize(N_sub = sum(N_vill))
+
+    # Join N_sub into the sae_vill dataset
+    sae_vill <- left_join(sae_vill, N_sub, by = c("Province", "Region", "Subdistrict"))
+
+    # Calculate Weight_agr_villsub (Nij/Ni) value
+    sae_vill <- sae_vill %>%
+      mutate(Weight_agr_villsub = N_vill / N_sub)
+
+    # Calculate y_agr_villsub per Subdistrict
+    vill_agr_sub <- sae_vill %>%
+      group_by(Province, Region, Subdistrict) %>%
+      summarize(y_agr_villsub = sum(Weight_agr_villsub * Y_eblup))
+
+    # Calculate variance per Subdistrict
+    var_agr_villsub <- sae_vill %>%
+      group_by(Province, Region, Subdistrict) %>%
+      summarise(var_agr_villsub = sum(Weight_agr_villsub^2 * MSE_eblup))
+
+    # Left join for the var_agr_villsub column in the vill_agr_sub dataset (to make it easier to calculate rse)
+    vill_agr_sub <- left_join(vill_agr_sub, dplyr::select(var_agr_villsub, Province, Region, Subdistrict, var_agr_villsub), by = c("Province", "Region", "Subdistrict"))
+
+    # Cakculate rse_agr_villsub
+    vill_agr_sub <- vill_agr_sub%>%
+      mutate(rse_agr_villsub = (sqrt(var_agr_villsub) / y_agr_villsub)*100)
+
+    # Join N_sub into vill_agr_sub dataset
+    vill_agr_sub <- left_join(vill_agr_sub, N_sub, by = c("Province", "Region", "Subdistrict"))
+
+    # Calculate N_reg (amount of data (population) in each Region)
+    N_reg <- vill_agr_sub %>%
+      group_by(Province, Region) %>%
+      summarize(N_reg = sum(N_sub))
+
+    # Join N_reg into the vill_agr_sub dataset
+    vill_agr_sub <- left_join(vill_agr_sub, N_reg, by = c("Province", "Region"))
+
+    # Calculate Weight_agr_subreg (Nij/Ni) value
+    vill_agr_sub <- vill_agr_sub%>%
+      mutate(Weight_agr_subreg = N_sub / N_reg)
+
+    # Show vill_agr_sub results for selected columns (user output)
+    result_villsub <- vill_agr_sub %>%
+      dplyr::select(Province, Region, Subdistrict, y_agr_villsub, rse_agr_villsub)
+
+    # Calculate y_agr_subreg per Region
+    sub_agr_reg <- vill_agr_sub %>%
+      group_by(Province, Region) %>%
+      summarize(y_agr_subreg = sum(Weight_agr_subreg * y_agr_villsub))
+
+    # Calculate var_agr_subreg
+    var_agr_subreg <- vill_agr_sub %>%
+      group_by(Province, Region) %>%
+      summarise(var_agr_subreg = sum(Weight_agr_subreg^2 * var_agr_villsub))
+
+    # Left join for the var_agr_subreg column in the sub_agr_reg dataset (to make it easy to calculate rse)
+    sub_agr_reg <- left_join(sub_agr_reg, dplyr::select(var_agr_subreg, Province, Region, var_agr_subreg), by = c("Province", "Region"))
+
+    # Calculate rse_agr_subreg
+    sub_agr_reg <- sub_agr_reg%>%
+      mutate(rse_agr_subreg = (sqrt(var_agr_subreg) / y_agr_subreg)*100)
+
+    # Join N_reg into sub_agr_reg dataset
+    sub_agr_reg <- left_join(sub_agr_reg, N_reg, by = c("Province", "Region"))
+
+    # Calculate N_prov (amount of data (population) in each Province)
+    N_prov <- sub_agr_reg %>%
+      group_by(Province) %>%
+      summarize(N_prov = sum(N_reg))
+
+    # Join N_prov into sub_agr_reg dataset
+    sub_agr_reg <- left_join(sub_agr_reg, N_prov, by = "Province")
+
+    # Calculate Weight_agr_regprov (Nij/Ni) value
+    sub_agr_reg <- sub_agr_reg%>%
+      mutate(Weight_agr_regprov = N_reg / N_prov)
+
+    # Show sub_agr_reg results for selected column (user output)
+    result_subreg <- sub_agr_reg %>%
+      dplyr::select(Province, Region, y_agr_subreg, rse_agr_subreg)
+
+    # Calculate y_agr_regprov per Province
+    reg_agr_prov <- sub_agr_reg %>%
+      group_by(Province) %>%
+      summarize(y_agr_regprov = sum(Weight_agr_regprov * y_agr_subreg))
+
+    # Calculate var_agr_regprov
+    var_agr_regprov <- sub_agr_reg %>%
+      group_by(Province) %>%
+      summarise(var_agr_regprov = sum(Weight_agr_regprov^2 * var_agr_subreg))
+
+    # Left join for the var_agr_regprov column in the reg_agr_prov dataset (to make it easy to calculate rse)
+    reg_agr_prov <- left_join(reg_agr_prov, dplyr::select(var_agr_regprov, Province, var_agr_regprov), by = "Province")
+
+    # Calculate rse_agr_regprov
+    reg_agr_prov <- reg_agr_prov%>%
+      mutate(rse_agr_regprov = (sqrt(var_agr_regprov) / y_agr_regprov)*100)
+
+    # Show reg_agr_prov results for selected column (user output)
+    result_regprov <- reg_agr_prov %>%
+      dplyr::select(Province, y_agr_regprov, rse_agr_regprov)
   }
-
-  # Extract eblup est, mse, and rse
-  y_eblup <- sae_result$est$eblup
-  mse_eblup <- sae_result$mse
-  rse_eblup <- (sqrt(mse_eblup) / y_eblup)*100
-
-  # Create the eblup_data dataset
-  eblup_data <- data.frame(Village = vill, Y_eblup = y_eblup, MSE_eblup = mse_eblup, RSE_eblup = rse_eblup)
-
-  # Create the sae_vill dataset
-  sae_vill <- data.frame(Province = prov, Region = reg, Subdistrict = sub, N_vill = N)
-
-  # Join the sae_vill dataset with eblup_data
-  sae_vill <- bind_cols(sae_vill, eblup_data)
-
-  # Calculate N_sub (amount of data (population) in each Subdistrict)
-  N_sub <- sae_vill %>%
-    group_by(Province, Region, Subdistrict) %>%
-    summarize(N_sub = sum(N_vill))
-
-  # Join N_sub into the sae_vill dataset
-  sae_vill <- left_join(sae_vill, N_sub, by = c("Province", "Region", "Subdistrict"))
-
-  # Calculate Weight_agr (Nij/Ni) value
-  sae_vill <- sae_vill %>%
-    mutate(Weight_agr = N_vill / N_sub)
-
-  # Calculate y_agr_villsub per Subdistrict
-  vill_agr_sub <- sae_vill %>%
-    group_by(Province, Region, Subdistrict) %>%
-    summarize(y_agr_villsub = sum(Weight_agr * Y_eblup))
-
-  # Calculate variance per Subdistrict
-  var_agr_villsub <- sae_vill %>%
-    group_by(Province, Region, Subdistrict) %>%
-    summarise(var_agr_villsub = sum(Weight_agr^2 * MSE_eblup))
-
-  # Left join for the var_agr_villsub column in the vill_agr_sub dataset (to make it easier to calculate rse)
-  vill_agr_sub <- left_join(vill_agr_sub, dplyr::select(var_agr_villsub, Province, Region, Subdistrict, var_agr_villsub), by = c("Province", "Region", "Subdistrict"))
-
-  # Cakculate rse_agr_villsub
-  vill_agr_sub <- vill_agr_sub%>%
-    mutate(rse_agr_villsub = (sqrt(var_agr_villsub) / y_agr_villsub)*100)
-
-  # Join N_sub into vill_agr_sub dataset
-  vill_agr_sub <- left_join(vill_agr_sub, N_sub, by = c("Province", "Region", "Subdistrict"))
-
-  # Calculate N_reg (amount of data (population) in each Region)
-  N_reg <- vill_agr_sub %>%
-    group_by(Province, Region) %>%
-    summarize(N_reg = sum(N_sub))
-
-  # Join N_reg into the vill_agr_sub dataset
-  vill_agr_sub <- left_join(vill_agr_sub, N_reg, by = c("Province", "Region"))
-
-  # Calculate Weight_agr (Nij/Ni) value
-  vill_agr_sub <- vill_agr_sub%>%
-    mutate(Weight_agr = N_sub / N_reg)
-
-  # Show vill_agr_sub results for selected columns (user output)
-  result_villsub <- vill_agr_sub %>%
-    dplyr::select(Province, Region, Subdistrict, y_agr_villsub, rse_agr_villsub)
-
-  # Calculate y_agr_subreg per Region
-  sub_agr_reg <- vill_agr_sub %>%
-    group_by(Province, Region) %>%
-    summarize(y_agr_subreg = sum(Weight_agr * y_agr_villsub))
-
-  # Calculate var_agr_subreg
-  var_agr_subreg <- vill_agr_sub %>%
-    group_by(Province, Region) %>%
-    summarise(var_agr_subreg = sum(Weight_agr^2 * var_agr_villsub))
-
-  # Left join for the var_agr_subreg column in the sub_agr_reg dataset (to make it easy to calculate rse)
-  sub_agr_reg <- left_join(sub_agr_reg, dplyr::select(var_agr_subreg, Province, Region, var_agr_subreg), by = c("Province", "Region"))
-
-  # Calculate rse_agr_subreg
-  sub_agr_reg <- sub_agr_reg%>%
-    mutate(rse_agr_subreg = (sqrt(var_agr_subreg) / y_agr_subreg)*100)
-
-  # Join N_reg into sub_agr_reg dataset
-  sub_agr_reg <- left_join(sub_agr_reg, N_reg, by = c("Province", "Region"))
-
-  # Calculate N_prov (amount of data (population) in each Province)
-  N_prov <- sub_agr_reg %>%
-    group_by(Province) %>%
-    summarize(N_prov = sum(N_reg))
-
-  # Join N_prov into sub_agr_reg dataset
-  sub_agr_reg <- left_join(sub_agr_reg, N_prov, by = "Province")
-
-  # Calculate Weight_agr (Nij/Ni) value
-  sub_agr_reg <- sub_agr_reg%>%
-    mutate(Weight_agr = N_reg / N_prov)
-
-  # Show sub_agr_reg results for selected column (user output)
-  result_subreg <- sub_agr_reg %>%
-    dplyr::select(Province, Region, y_agr_subreg, rse_agr_subreg)
-
-  # Calculate y_agr_regprov per Province
-  reg_agr_prov <- sub_agr_reg %>%
-    group_by(Province) %>%
-    summarize(y_agr_regprov = sum(Weight_agr * y_agr_subreg))
-
-  # Calculate var_agr_regprov
-  var_agr_regprov <- sub_agr_reg %>%
-    group_by(Province) %>%
-    summarise(var_agr_regprov = sum(Weight_agr^2 * var_agr_subreg))
-
-  # Left join for the var_agr_regprov column in the reg_agr_prov dataset (to make it easy to calculate rse)
-  reg_agr_prov <- left_join(reg_agr_prov, dplyr::select(var_agr_regprov, Province, var_agr_regprov), by = "Province")
-
-  # Calculate rse_agr_regprov
-  reg_agr_prov <- reg_agr_prov%>%
-    mutate(rse_agr_regprov = (sqrt(var_agr_regprov) / y_agr_regprov)*100)
-
-  # Show reg_agr_prov results for selected column (user output)
-  result_regprov <- reg_agr_prov %>%
-    dplyr::select(Province, y_agr_regprov, rse_agr_regprov)
 
   result$Est_Area3 = result_villsub
   result$Est_Area2 = result_subreg
